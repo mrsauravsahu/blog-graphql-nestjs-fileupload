a2053200dbb627482468132a9484530366b84cbb
Saurav Sahu <mrsauravsahu@outlook.com>
2021-10-16 17:05:07 +0530

    add startup message

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 8080;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  console.log(`App running at ${await app.getUrl()}`);
 }
 bootstrap();
