ddb95a1b44bd123365bb27e05de040c39c09da31
Saurav Sahu <mrsauravsahu@outlook.com>
2021-10-18 10:45:41 +0530

    save file to drive post upload

--- src/modules/person/person.resolver.ts
+++ src/modules/person/person.resolver.ts
@@ -36,6 +36,7 @@ export class PersonResolver {
         stream.on('data', function (chunk) {
           chunks.push(chunk);
         });
+
         stream.on('end', function () {
           buffer = Buffer.concat(chunks);
           resolve(buffer);
@@ -47,7 +48,7 @@ export class PersonResolver {
       const base64 = buffer.toString('base64');
       // If you want to store the file, this is one way of doing
       // it, as you have the file in-memory as Buffer
-      // await fs.writeFile('upload.jpg', buffer);
+      await fs.writeFile('upload.jpg', buffer);
       this.person.coverPhotoLength = base64.length;
       this.person.coverPhoto = base64;
 
